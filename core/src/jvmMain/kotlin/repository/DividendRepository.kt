dee9e546653b50d72276746259de35febad9367b
Andrew Sheragy <andy@cerion.org>
2022-06-29 22:45:35 -0700

    removed repository classes

--- core/src/jvmMain/kotlin/repository/DividendRepository.kt
+++
@@ -1,9 +0,0 @@
-package org.cerion.marketdata.core.repository
-
-import org.cerion.marketdata.core.model.Dividend
-
-interface DividendRepository {
-    fun get(symbol: String): List<Dividend>
-    fun add(symbol: String, list: List<Dividend>)
-    fun deleteAll()
-}
